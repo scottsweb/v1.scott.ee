bf0360d4e6a21bab125779a1bd06ac550de0a50f
Scott Evans <git@scott.ee>
2018-05-27 21:54:04 +0100

    Respect data saving settings and reduce image quality in low bandwidth situations

--- plugins/lazyload.js
+++ plugins/lazyload.js
@@ -5,12 +5,37 @@ Vue.use( VueLazyload, {
 	preLoad: 1.0,
 	throttleWait: 600,
 	filter: {
-		progressive (listener, options) {
+		progressive( listener, options ) {
 			const isCDN = /wp\.com/
 			const regexTypeOne = /w=(\d)*/gm;
+			// use a low res image for the loader
 			if ( isCDN.test( listener.src ) ) {
 				listener.el.setAttribute( 'lazy-progressive', 'true' )
 				listener.loading = listener.src.replace( regexTypeOne, '' ) + '&w=90'
+			}
+			// respect data save header and improve low bandwidth performance
+			if ( 'connection' in navigator && isCDN.test( listener.src ) ) {
+				if (
+					navigator.connection.saveData === true ||
+					navigator.connection.effectiveType === 'slow-2g' ||
+					navigator.connection.effectiveType === '2g' ||
+					navigator.connection.effectiveType === '3g'
+				) {
+					const regexSize = /\?(resize|w)=(\d*)/gmi;
+					const dataSrc = listener.src
+					const dataSrcset = listener.el.getAttribute( 'data-srcset' )
+
+					const newDataSrc = dataSrc.replace( regexSize, ( match, $1, $2 ) => {
+						return match.replace( 'resize', 'w' ).replace( $2, ( $2 / 4 ) * window.devicePixelRatio )
+					} );
+
+					const newDataSrcset = dataSrcset.replace( regexSize, ( match, $1, $2 ) => {
+						return match.replace( $2, ( $2 / 4 ) * window.devicePixelRatio )
+					} );
+
+					listener.src = newDataSrc
+					listener.el.setAttribute( 'data-srcset', newDataSrcset )
+				}
 			}
 		}
 	}
