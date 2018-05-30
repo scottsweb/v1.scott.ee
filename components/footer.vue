fc50a6c4002e8de9529c61d46ca5ab593b84dfca
Scott Evans <git@scott.ee>
2018-05-30 20:41:54 +0100

    Link to correct dat location

--- components/footer.vue
+++ components/footer.vue
@@ -9,7 +9,7 @@
 			<li><a href="https://scott.ee/feed/">RSS</a></li>
 			<li><!--Inital load in {{ generated }} seconds--></li>
 		</ul>
-		<a class="dat-link tooltip" href="dat://scott.ee/" data-tooltip="P2P/Dat Version">
+		<a class="dat-link tooltip" :href="dat" data-tooltip="P2P/Dat Version">
 			<span class="screen-reader-text">A P2P/Dat version of this website is also available</span>
 			<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 240 240">
 				<path d="M120 240a9 9 0 0 1-4.5-1.21l-96-55.5a9 9 0 0 1-4.5-7.79v-111a9 9 0 0 1 4.5-7.79l96-55.5a9 9 0 0 1 9 0l96 55.5a9 9 0 0 1 4.5 7.79v111a9 9 0 0 1-4.5 7.79l-96 55.5A9 9 0 0 1 120 240zm-87-69.69l87 50.3 87-50.3V69.69L120 19.4 33 69.69z"/>
@@ -40,6 +40,9 @@ export default {
 				return;
 			}
 			return ( t.loadEventEnd - t.navigationStart) / 1000;
+		},
+		dat() {
+			return 'dat://scott.ee' + this.$route.path
 		}
 	}
 }
