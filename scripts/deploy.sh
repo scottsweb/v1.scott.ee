91af94d483f898f64bb6a7e753fcc20afc627769
Scott Evans <git@scott.ee>
2018-05-31 07:51:45 +0100

    Rough deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+# TODO: https://stackoverflow.com/a/246128
+# generate the app - switch to content only
+npm run generate
+
+# remove node modules from build  (needs investigating)
+rm -rf dist/node_modules/
+
+# copy dist into dat archive
+# dat sync will be running and pick up changes
+cp dist/. ../scott.ee.dat/ -r
+
+# deploy to Netlify
+# 104.198.14.52
+netlifyctl deploy -b dist/
+
+# deploy to GitHub
+# 185.199.109.153
+npm run deploy-gh
